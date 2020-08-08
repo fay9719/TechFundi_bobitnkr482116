7e6922055646068f614f84c8ae4e1669256b8c55
fay9719 <69317579+fay9719@users.noreply.github.com>
2020-08-08 22:19:52 +0300

    Update IP_locater_2.py

--- IP_locater_2.py
+++ IP_locater_2.py
@@ -9,16 +9,16 @@ def my_ip_location(my_ip):
     location = reader.get(my_ip)
 
     #geolite database dict values and fine tunning
-    a=(location['city']['names']['en'])
-    b=(location['continent']['names']['en'])
-    c=(location['country']['names']['en'])
-    d=(location['location'])
-    e=(location['postal'])
-    f=(location['registered_country']['names']['en'])
-    g=(location['subdivisions'][0]['names']['en'])
+    1=(location['city']['names']['en'])
+    2=(location['continent']['names']['en'])
+    3=(location['country']['names']['en'])
+    4=(location['location'])
+    5=(location['postal'])
+    6=(location['registered_country']['names']['en'])
+    7=(location['subdivisions'][0]['names']['en'])
 
     print('''city: %s\ncontinent: %s\ncountry: %s\nlocation: %s\npostal: %s\nregistered_country: %s\nsubdivisions: %s\n'''
-     % (a,b,c,d,e,f,g))
+     % (1,2,,4,5,6,7))
 
 
 my_ip = requests.get('https://api.ipify.org').text
